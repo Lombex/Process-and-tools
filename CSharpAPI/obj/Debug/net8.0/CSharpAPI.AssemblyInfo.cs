0c290749e08bbce4669686ac5e9dd03ecd926a50
Zdennick <LombexTV@gmail.com>
2024-10-16 13:33:46 +0200

    Added all necessary Controller .cs files

--- CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
+++ CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c5bc19cf8fb365c459dc29bb0019c45d6c8d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1300e9a94dad006572a9f01f9ced8aaab750a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
