dbde0bec1e4bf7e6d7ab48a6ba834111023cdbe0
Lombex <LombexTV@gmail.com>
2024-10-20 16:03:34 +0200

    added all data models to the project

---
+++ CSharpAPI/Models/Orders.cs
@@ -0,0 +1,30 @@
+namespace CSharpAPI.Models 
+{
+    public class OrdersModel 
+    {
+        public int id { get; set; }
+        public int source_id { get; set; }
+        public string? order_date { get; set; }
+        public string? request_date { get; set; }
+        public string? reference { get; set; }
+        public string? reference_extra { get; set; }
+        public string? order_status { get; set; }
+        public string? notes { get; set; }
+        public string? shipping_notes { get; set; }
+        public string? picking_notes { get; set; }
+        public int warehouse_id { get; set; }
+
+        // ship_to { get; set; }
+        // bill_to { get; set; }
+
+        public int shipment_id { get; set; }
+        public float total_amount { get; set; }
+        public float total_discount { get; set; }
+        public float total_tax { get; set; }
+        public float total_surcharge { get; set; }
+
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public List<Items>? items { get; set; }
+    }
+}
--- CSharpAPI/Models/Warehouse.cs
+++ CSharpAPI/Models/Warehouse.cs
@@ -4,22 +4,22 @@ namespace CSharpAPI.Models
     public class Warehouse
     {
         public int Id { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Zip { get; set; }
-        public string City { get; set; }
-        public string Province { get; set; }
-        public string Country { get; set; }
-        public Contact Contact { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Zip { get; set; }
+        public string? City { get; set; }
+        public string? Province { get; set; }
+        public string? Country { get; set; }
+        public Contact? Contact { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
 
     public class Contact
     {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
     }
 }
