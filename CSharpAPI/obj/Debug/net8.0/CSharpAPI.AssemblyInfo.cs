b6abeb9d25b04c016907ca3f7dce8aec93bf1101
Steyn229 <steyn081@hotmail.com>
2024-12-03 21:49:30 +0100

    .

--- CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
+++ CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+262a5206fd11e7f289b6ac0d78be18e30687da06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df78f045b95578e0ba15c577dc2f0ddca7f15dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
