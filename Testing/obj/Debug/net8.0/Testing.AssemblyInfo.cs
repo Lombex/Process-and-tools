a85174057a4f435bf4733e2b1d1c8176a3aad7a8
Steyn229 <steyn081@hotmail.com>
2024-12-16 22:10:48 +0100

    .

--- Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae2e26fd05c9886304a0c825946a9bbbe5d1b435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fdc2594e2d5150f33ee2428f3dfe457f32ef8d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
