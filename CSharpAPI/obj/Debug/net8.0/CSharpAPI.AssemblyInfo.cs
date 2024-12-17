94ebf6486666cff2f7322976b62bd9eda6973742
Steyn229 <steyn081@hotmail.com>
2024-12-17 10:35:56 +0100

    check

--- CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
+++ CSharpAPI/obj/Debug/net8.0/CSharpAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9a2804d5be3d7832c0a50da8d750dfde123b2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16f9d629aa8f4c82ab7f8eef8abf30acb1daeda")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
